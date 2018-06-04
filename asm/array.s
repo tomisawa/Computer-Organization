997c72240cfdd2671b718941dfaf673d4fa08ee1
tomisawa <tomisawa@maebashi-it.ac.jp>
2018-06-04 15:38:22 +0900

    インデントの訂正

--- asm/array.s
+++ asm/array.s
@@ -14,12 +14,12 @@ A3:.word 4
 # i:$s0, $s5=&A[0]
 #
 __start:
-		la $s5,A  # $s5=&A[0]
-		addi $s0,$zero,2 	# i=2
-		sll $t0,$s0,2
-		add $t0,$t0,$s5
-    lw $t1,0($t0)			# $t1=A[i]
-		addi $t1,$t1,10  	# $t1=A[i]+10
-		sw $t1,0($t0)			# A[i]=$1
+    la $s5,A  # $s5=&A[0]
+    addi $s0,$zero,2 	# i=2
+    sll $t0,$s0,2
+    add $t0,$t0,$s5
+    lw $t1,0($t0)	# $t1=A[i]
+    addi $t1,$t1,10  	# $t1=A[i]+10
+    sw $t1,0($t0)	# A[i]=$1
     nop
     break 2
